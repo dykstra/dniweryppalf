5a806774cbb7027cca961ce315ce8cd2277d70bf
Doug Dykstra <dykstra.doug@gmail.com>
2014-05-18 22:34:18 -0400

    changed images / added button images

--- FlappyRewind/HighScoreViewController.m
+++ FlappyRewind/HighScoreViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     HighScoreNumber = [[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"];
-    HighScore.text = [NSString stringWithFormat:@"High Score: %li", (long)HighScoreNumber];
+    HighScore.text = [NSString stringWithFormat:@" %li", (long)HighScoreNumber];
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
